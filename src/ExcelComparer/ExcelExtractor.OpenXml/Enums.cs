7858ad3fb8fac217badd1dc94cfc8c0e245db749
Phong Nguyen <nguyendoanphong.it@gmail.com>
2024-12-31 16:29:44 +0700

    OpenXml

---
+++ src/ExcelComparer/ExcelExtractor.OpenXml/Enums.cs
@@ -0,0 +1,79 @@
+﻿namespace ExcelExtractor.OpenXml;
+
+public enum ThemeColor
+{
+    Background1,
+    Text1,
+    Background2,
+    Text2,
+    Accent1,
+    Accent2,
+    Accent3,
+    Accent4,
+    Accent5,
+    Accent6,
+    Hyperlink,
+    FollowedHyperlink
+}
+
+public enum FillPatternType
+{
+    DarkDown,
+    DarkGray,
+    DarkGrid,
+    DarkHorizontal,
+    DarkTrellis,
+    DarkUp,
+    DarkVertical,
+    Gray0625,
+    Gray125,
+    LightDown,
+    LightGray,
+    LightGrid,
+    LightHorizontal,
+    LightTrellis,
+    LightUp,
+    LightVertical,
+    MediumGray,
+    None,
+    Solid
+}
+
+public enum AlignmentHorizontal
+{
+    Center,
+    CenterContinuous,
+    Distributed,
+    Fill,
+    General,
+    Justify,
+    Left,
+    Right
+}
+
+public enum AlignmentVertical
+{
+    Bottom,
+    Center,
+    Distributed,
+    Justify,
+    Top
+}
+
+public enum BorderStyle
+{
+    DashDot,
+    DashDotDot,
+    Dashed,
+    Dotted,
+    Double,
+    Hair,
+    Medium,
+    MediumDashDot,
+    MediumDashDotDot,
+    MediumDashed,
+    None,
+    SlantDashDot,
+    Thick,
+    Thin
+}
--- src/ExcelComparer/ExcelExtractor.OpenXml/Extentions.cs
+++ src/ExcelComparer/ExcelExtractor.OpenXml/Extentions.cs
@@ -37,6 +37,13 @@ public static class Extentions
 
         var fill = (Fill)stylesheet.Fills.ElementAt(int.Parse(style.FillId));
 
+        var border = (Border)stylesheet.Borders.ElementAt(int.Parse(style.BorderId));
+
+        if (cell.CellReference == "B21")
+        {
+
+        }
+
         var rs = new Abstractions.Style
         {
             Font = new Abstractions.Font
@@ -47,37 +54,37 @@ public static class Extentions
             },
             Fill = new Abstractions.Fill
             {
-                PatternType = fill.PatternFill.PatternType,
+                PatternType = Enum.Parse(typeof(FillPatternType), fill.PatternFill.PatternType, ignoreCase: true).ToString(),
                 BackgroundColor = fill.PatternFill.BackgroundColor.GetColor(document)
             },
             Alignment = new Abstractions.Alignment
             {
-                Horizontal = style.Alignment?.Horizontal?.InnerText,
-                Vertical = style.Alignment?.Vertical?.InnerText
+                Horizontal = Enum.Parse(typeof(AlignmentHorizontal), style.Alignment?.Horizontal?.InnerText ?? "General", ignoreCase: true).ToString(),
+                Vertical = Enum.Parse(typeof(AlignmentVertical), style.Alignment?.Vertical?.InnerText ?? "Bottom", ignoreCase: true).ToString()
             },
-            //Border = new Border
-            //{
-            //    Top = new BorderItem
-            //    {
-            //        Color = cell.Style.Border.TopBorderColor.GetColor(),
-            //        Style = cell.Style.Border.TopBorder.ToString()
-            //    },
-            //    Left = new BorderItem
-            //    {
-            //        Color = cell.Style.Border.LeftBorderColor.GetColor(),
-            //        Style = cell.Style.Border.LeftBorder.ToString()
-            //    },
-            //    Bottom = new BorderItem
-            //    {
-            //        Color = cell.Style.Border.BottomBorderColor.GetColor(),
-            //        Style = cell.Style.Border.BottomBorder.ToString()
-            //    },
-            //    Right = new BorderItem
-            //    {
-            //        Color = cell.Style.Border.RightBorderColor.GetColor(),
-            //        Style = cell.Style.Border.RightBorder.ToString()
-            //    }
-            //},
+            Border = new Abstractions.Border
+            {
+                Top = new Abstractions.BorderItem
+                {
+                    Color = border.TopBorder.Color.GetColor(document),
+                    Style = FormatBorderStyle(border.TopBorder.Style.ToString())
+                },
+                Left = new Abstractions.BorderItem
+                {
+                    Color = border.LeftBorder.Color.GetColor(document),
+                    Style = FormatBorderStyle(border.LeftBorder.Style.ToString())
+                },
+                Bottom = new Abstractions.BorderItem
+                {
+                    Color = border.BottomBorder.Color.GetColor(document),
+                    Style = FormatBorderStyle(border.BottomBorder.Style.ToString())
+                },
+                Right = new Abstractions.BorderItem
+                {
+                    Color = border.RightBorder.Color.GetColor(document),
+                    Style = FormatBorderStyle(border.RightBorder.Style.ToString())
+                }
+            },
             //Numberformat = new Numberformat
             //{
             //    Format = cell.Style.NumberFormat.Format
@@ -90,6 +97,28 @@ public static class Extentions
 
     public static Abstractions.Color GetColor(this Color color, SpreadsheetDocument document)
     {
+        if (color == null)
+        {
+            return null;
+        }
+
+        var colorModel = new Abstractions.Color();
+
+        if (color.Theme != null)
+        {
+            colorModel.Theme = ((ThemeColor)(int)color.Theme.Value).ToString();
+        }
+
+        colorModel.Tint = color.Tint != null ? (decimal)color.Tint.Value : null;
+        colorModel.Indexed = color.Indexed != null ? (int)color.Indexed.Value : null;
+        colorModel.Rgb = color.Rgb?.Value;
+
+
+        return colorModel;
+    }
+
+    public static Abstractions.Color GetColor(this BackgroundColor color, SpreadsheetDocument document)
+    {
 
         if (color == null)
         {
@@ -111,26 +140,8 @@ public static class Extentions
         return colorModel;
     }
 
-    public static Abstractions.Color GetColor(this DocumentFormat.OpenXml.Spreadsheet.BackgroundColor color, SpreadsheetDocument document)
+    private static string FormatBorderStyle(this string style)
     {
-
-        if (color == null)
-        {
-            return null;
-        }
-
-        var colorModel = new Abstractions.Color();
-
-        if (color.Theme != null)
-        {
-            colorModel.Theme = ((ThemeColor)(int)color.Theme.Value).ToString();
-        }
-
-        colorModel.Tint = color.Tint != null ? (decimal)color.Tint.Value : null;
-        colorModel.Indexed = color.Indexed != null ? (int)color.Indexed.Value : null;
-        colorModel.Rgb = color.Rgb?.Value;
-
-
-        return colorModel;
+        return Enum.Parse(typeof(BorderStyle), style, ignoreCase: true).ToString();
     }
 }
