7e15d56b76008a54ffc7ca4f0da4886af9b16a3c
Phong Nguyen <nguyendoanphong.it@gmail.com>
2025-06-08 11:51:50 +0700

    Excel Formula

---
+++ src/ExcelFormula/ExcelFormula.ClosedXml/Program.cs
@@ -0,0 +1,15 @@
+﻿using ClosedXML.Excel;
+
+var workbook = new XLWorkbook($"../../../../Template.xlsx");
+var worksheet = workbook.Worksheet("Sheet1");
+
+//var sum = worksheet.Evaluate("SUM(A1,B1)");
+
+worksheet.Cell("A1").Value = 1;
+worksheet.Cell("B1").Value = 5;
+
+workbook.RecalculateAllFormulas();
+
+var num = worksheet.Cell("B2").Value.ToString();
+
+Console.WriteLine($"The sum of A1 and B1 is: {num}");
---
+++ src/ExcelFormula/ExcelFormula.EPPlus/Program.cs
@@ -0,0 +1,15 @@
+﻿using OfficeOpenXml;
+
+ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+var excelPackage = new ExcelPackage($"../../../../Template.xlsx");
+var worksheet = excelPackage.Workbook.Worksheets["Sheet1"];
+
+worksheet.Cells["A1"].Value = 1;
+worksheet.Cells["B1"].Value = 5;
+
+worksheet.Calculate();
+
+var num = worksheet.Cells["B2"].Value.ToString();
+
+Console.WriteLine($"The sum of A1 and B1 is: {num}");
